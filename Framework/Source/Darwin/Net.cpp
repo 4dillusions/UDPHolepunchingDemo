032fcf6e1da3bf6c62de4df6232224ecfdb83c5e
Robert Oze <support@4dillusions.com>
2024-06-24 02:08:48 +0200

    add macos (Darwin) platform

---
+++ Framework/Source/Darwin/Net.cpp
@@ -0,0 +1,18 @@
+/*
+UDP Holepunching demo
+Copyright (c) 2020 by 4D Illusions. All rights reserved.
+Released under the terms of the GNU General Public License version 3 or later.
+*/
+
+#include "../Net.h"
+
+namespace Network
+{
+	bool Net::Init()
+	{
+		return true;
+	}
+
+	void Net::Cleanup()
+	{ }
+}
---
+++ Framework/Source/Darwin/SocketAddressPlatform.cpp
@@ -0,0 +1,33 @@
+/*
+UDP Holepunching demo
+Copyright (c) 2020 by 4D Illusions. All rights reserved.
+Released under the terms of the GNU General Public License version 3 or later.
+*/
+
+#include "../SocketAddress.h"
+#include "../IpV4Address.h"
+
+namespace Network
+{	
+	string SocketAddress::GetLocalIp()
+	{
+		setenv("LANG", "C", 1);
+
+		FILE* fp = popen("ifconfig | grep 'inet ' | grep -v 127.0.0.1 | awk '{print $2}'", "r");
+		if (fp)
+		{
+			char* ip = nullptr;
+			size_t count;
+			getline(&ip, &count, fp);
+			string result(ip);
+
+			if (!result.empty() && result.back() == '\n')
+				result.pop_back();
+
+			return result;
+		}
+		pclose(fp);
+
+		return string();
+	}
+}
